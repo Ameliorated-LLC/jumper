ed97a66d7e224d8ee1484390095372ebc53d931e
Styris <styris@styris.net>
2024-11-10 10:17:46 -0800

    Update PPA

--- Packaging/build_ppa.sh
+++ Packaging/build_ppa.sh
@@ -3,6 +3,8 @@ if [[ -z "$1" || ! "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     exit 1
 fi
 
+CURDIR="$(pwd)"
+
 command -v gpg > /dev/null 2>&1 || { echo "Error: gpg command is not available" >&2; exit 1; }
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -12,27 +14,30 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm -rf "/tmp/JumpPPA"
 cp -rf "$SCRIPT_DIR/PPA" "/tmp/JumpPPA"
 
+cd /tmp/JumpPPA
 # Generate Packages file and compress it
-dpkg-scanpackages --multiversion "/tmp/JumpPPA" > "/tmp/JumpPPA/Packages"
-gzip -k -f "/tmp/JumpPPA/Packages"
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
 
 # Generate Release file
-apt-ftparchive release "/tmp/JumpPPA" > "/tmp/JumpPPA/Release"
+apt-ftparchive release . > Release
 
-gpg "/tmp/JumpPPA/ppa-private-key.asc.gpg"
+gpg "ppa-private-key.asc.gpg"
 
 export GNUPGHOME=$(mktemp -d)
 
-gpg --import "/tmp/JumpPPA/ppa-private-key.asc"
+gpg --import "ppa-private-key.asc"
 
-rm -f "/tmp/JumpPPA/ppa-private-key.asc"
+rm -f "ppa-private-key.asc"
 
-gpg --local-user "styris_packaging@fastmail.com" -abs -o - "/tmp/JumpPPA/Release" > "/tmp/JumpPPA/Release.gpg"
-gpg --local-user "styris_packaging@fastmail.com" --clearsign -o - "/tmp/JumpPPA/Release" > "/tmp/JumpPPA/InRelease"
+gpg --local-user "styris_packaging@fastmail.com" -abs -o - Release > Release.gpg
+gpg --local-user "styris_packaging@fastmail.com" --clearsign -o - Release > InRelease
 
 rm -rf "$GNUPGHOME"
 
 cp -rf /tmp/JumpPPA/* "$SCRIPT_DIR/PPA"
 rm -rf "/tmp/JumpPPA"
+
+cd "$CURDIR"
 
 echo "PPA build completed. Please commit and push changes to make release publicly available."
